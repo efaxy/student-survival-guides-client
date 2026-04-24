f078ff52f10c42d4833d57d75f4ffb4b76bb766f
yuma <nak071104@gmail.com>
2026-04-24 12:29:06 +0500

    implement PostsPage component to display user posts with associated styles

--- src/pages/PostsPage/PostsPage.jsx
+++ src/pages/PostsPage/PostsPage.jsx
@@ -1,5 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { PostItem } from '../../components/PostItem/PostItem'
+import axios from 'axios'
+import './PostsPage.css'
 
 export const PostsPage = () => {
-	return <div>PostsPage</div>
+	const [posts, setPosts] = useState([])
+
+	const fetchMyPosts = async () => {
+		try {
+			const { data } = await axios.get('/posts/user/me')
+			setPosts(data)
+		} catch (error) {
+			console.log(error)
+		}
+	}
+
+	useEffect(() => {
+		fetchMyPosts()
+	}, [])
+
+	return (
+		<div className="posts-page-container">
+			{posts?.map((post, idx) => (
+				<PostItem post={post} key={idx} />
+			))}
+		</div>
+	)
 }
