e4771064ce4aff69a42598d39c2ca69146ed18fb
yuma <nak071104@gmail.com>
2026-04-24 12:57:38 +0500

    initialize core application routing and apply base global styles

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import { Layout } from './components/Layout/Layout'
+import React, { useEffect } from 'react'
 import { Route, Routes } from 'react-router-dom'
-
+import { Layout } from './components/Layout/Layout'
 import { MainPage } from './pages/MainPage/MainPage'
 import { PostsPage } from './pages/PostsPage/PostsPage'
 import { PostPage } from './pages/PostPage/PostPage'
@@ -8,15 +8,26 @@ import { AddPostPage } from './pages/AddPostPage/AddPostPage'
 import { RegisterPage } from './pages/RegisterPage/RegisterPage'
 import { LoginPage } from './pages/LoginPage/LoginPage'
 import { EditPostPage } from './pages/EditPostPage/EditPostPage'
-import { useDispatch } from 'react-redux'
-import { useEffect } from 'react'
+import axios from 'axios'
 
 function App() {
-	const dispatch = useDispatch()
-
 	useEffect(() => {
-		dispatch(getMe())
-	}, [dispatch])
+		const checkAuth = async () => {
+			const token = window.localStorage.getItem('token')
+			if (token) {
+				try {
+					// Проверка токена при загрузке приложения
+					await axios.get('/auth/me', {
+						headers: { Authorization: `Bearer ${token}` }
+					})
+				} catch (error) {
+					console.log('Token expired or invalid')
+					window.localStorage.removeItem('token')
+				}
+			}
+		}
+		checkAuth()
+	}, [])
 
 	return (
 		<Layout>
