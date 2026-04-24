fa549fcc843462092ef8fb1154a18d83e8eb93db
yuma <nak071104@gmail.com>
2026-04-24 12:47:24 +0500

    add PopularPosts component to display post navigation links

--- src/components/PopularPosts/PopularPosts.jsx
+++ src/components/PopularPosts/PopularPosts.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import './PopularPosts.css'
 
 export const PopularPosts = ({ post }) => {
-	return (
-		<div className="popular-post-item">
-			<div className="popular-post-link">{post.popular}</div>
-		</div>
-	)
+    return (
+        <div className="popular-post-item">
+            <Link
+                to={`${post._id}`}
+                className="popular-post-link"
+            >
+                {post.title}
+            </Link>
+        </div>
+    )
 }
